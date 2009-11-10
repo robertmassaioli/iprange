379a5b5cd7b7fadad4ed53929fdf129b2a1c3f2a
Robert Massaioli <robertmassaioli@gmail.com>
2009-11-10 17:37:49 +1100

    Adding the add() function
    I am starting to add the basic parsing of the add functions.

--- iprange.cpp
+++ iprange.cpp
@@ -1,26 +1,76 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <cctype>
+#include <climits>
 #include "iprange.h"
 
 using namespace std;
 
-void IPRange::add(const char* ipaddr) {
-   // break the address up into ranges
+IPRange::IPRange(int rangeSize) {
+   r_size = rangeSize;
+   cout << "Created IPRange (size:" << r_size << ")" << endl;
+}
+
+int IPRange::add(const char* ipaddr) {
+   // converting from a const char* to a char*
    short int strLen = strlen(ipaddr);
    char *ipadd = (char*)malloc(strLen + 1);
    strncpy(ipadd, ipaddr, strLen + 1);
 
+   // breaking the string up by . separators
    cout << "Breaking up: " << ipaddr << endl;
    char *tok = NULL;
+   char **gaps = (char**)malloc (sizeof(char*) * r_size);
+   short int i = 0;
    tok = strtok(ipadd, ".");
    while(NULL != tok) {
-      cout << tok << endl;
+      strLen = strlen(tok);
+      gaps[i] = (char*)malloc(strLen + 1);
+      strncpy(gaps[i], tok, strLen + 1);
 
+      cout << gaps[i] << endl;
       tok = strtok(NULL, ".");
+      i++;
    } 
 
+   free(ipadd);
+
+   Range* ranges[r_size];
+   Range* tempRange = NULL;
+   int minIn, maxIn;
+   for (i = 0; i < r_size; i++) {
+      if (isdigit(*gaps[i])) {
+         minIn = atoi(gaps[i]);
+         if (minIn == INT_MAX || minIn == INT_MIN) return INVALID_NAN;
+         if (minIn < 0 || minIn > 0xFF) return INVALID_NOT_IN_RANGE;
+         cout << "Single: " << minIn << endl;
+         tempRange = new Range((char)minIn, (char)minIn);
+      } else {
+         cout << "Range: " << gaps[i] << endl;
+         tok = strtok(gaps[i], "[]-");
+         minIn = -1;
+         while (NULL != tok) {
+            if (minIn < 0) {
+               minIn = atoi(tok);
+            } else {
+               maxIn = atoi(tok);
+            }
+            tok = strtok(NULL, "[]-");
+         }
+         if (maxIn == INT_MAX || maxIn == INT_MIN) return INVALID_NAN;
+         if (minIn == INT_MAX || minIn == INT_MIN) return INVALID_NAN;
+         if (maxIn < 0 || maxIn > 0xFF) return INVALID_NOT_IN_RANGE;
+         if (minIn < 0 || minIn > 0xFF) return INVALID_NOT_IN_RANGE;
+         tempRange = new Range((char)minIn, (char)maxIn);
+      }
+      ranges[i] = tempRange;
+   }
+   free(gaps);    // dont need the gaps anymore
+
    // add the ranges up into the tree and merge where required
+   //
+   return SUCCESSFUL_ADD;
 }
 
 bool IPRange::includes(const char* idaddr) {
--- iprange.h
+++ iprange.h
@@ -3,17 +3,28 @@
 
 #include <vector>
 
-class range {
+#define IPRANGE_SIZE_IPV4  4
+#define IPRANGE_SIZE_IPV6  8
+
+#define SUCCESSFUL_ADD        0
+#define INVALID_NAN           1
+#define INVALID_NOT_IN_RANGE  2
+
+class Range {
    public:
+      Range(unsigned char s = 0, unsigned char e = 0) {start = s; end = e;}
       unsigned char start, end;
 };
 
 class IPRange {
    public:
-      void add(const char* ipaddr);
+      IPRange (int rangeSize = IPRANGE_SIZE_IPV4);
+      // returns error code
+      int add(const char* ipaddr);
       bool includes(const char* ipaddr);
    private:
-      std::vector<std::vector<range*> > fullRange;
+      std::vector<std::vector<Range*> > fullRange;
+      short int r_size;
 };
 
 #endif
